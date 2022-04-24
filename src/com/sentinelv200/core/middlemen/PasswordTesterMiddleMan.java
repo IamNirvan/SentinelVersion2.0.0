b1fa7682e79bb41f08be4c2993d0417cf6aeb675
Nirvan <nirvan.work@gmail.com>
2022-04-24 22:22:42 +0500

    Update PasswordTesterMiddleMan.java
    Implemented the start method and createEngine method

--- src/com/sentinelv200/core/middlemen/PasswordTesterMiddleMan.java
+++ src/com/sentinelv200/core/middlemen/PasswordTesterMiddleMan.java
@@ -1,29 +1,36 @@
 package com.sentinelv200.core.middlemen;
 
 import com.sentinelv200.core.Core;
+import com.sentinelv200.engines.passwordtester.Affire;
 import com.sentinelv200.engines.passwordtester.PasswordTester;
 
 public class PasswordTesterMiddleMan implements MiddleMan{
     PasswordTester passwordTester;
+    private final String GENERATOR_NAME = "Affire";
 
-    @Override
-    public boolean locateEngine() {
-        return false;
+    // The event handler for a button or whatever, will invoke this method.
+    public void start() {
+        createEngine();
+        addEngine();
+        createForm();
     }
 
     @Override
     public void createEngine() {
-        
+        if(getEngineActivationStatus(GENERATOR_NAME)) {
+            this.passwordTester = new Affire();
+        }        
     }
 
     @Override
     public void createForm() {
-        
     }
 
     @Override
     public void addEngine() {
-        Core.addEngine(passwordTester);        
+        if(this.passwordTester != null) {
+            Core.addEngine(passwordTester);        
+        }
     }
 
     @Override
