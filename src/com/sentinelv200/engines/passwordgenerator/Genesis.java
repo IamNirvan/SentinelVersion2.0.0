06d65858b6f3e71154461a7115110e9a32bf6365
Nirvan <nirvan.work@gmail.com>
2022-04-25 19:27:33 +0500

    Update Genesis.java
    Added doc strings, implemented the generatePassword method and set getters and setters

--- src/com/sentinelv200/engines/passwordgenerator/Genesis.java
+++ src/com/sentinelv200/engines/passwordgenerator/Genesis.java
@@ -1,44 +1,62 @@
 package com.sentinelv200.engines.passwordgenerator;
 
 import com.sentinelv200.Utility.ToolBox;
-
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Random;
 import java.util.Set;
-import javax.swing.JTextField;
 
 /**
- *
  * @author Shalin Kulawardane
  */
-public class Genesis extends Thread implements PasswordGenerator{
-    private final Set<Character> SERVICE_NAME;
+public class Genesis implements PasswordGenerator{
+    private Set<Character> serviceName;
     private final int PASSWORD_LENGTH;
-    public final int[] SEGMENT_LENGTHS;
-    public byte[] result;
-    private final JTextField TEXT_FIELD;
+    private final int[] SEGMENT_LENGTHS;
     
-    public Genesis(Set<Character> serviceName, int length, JTextField textField) {
-        this.TEXT_FIELD = textField;
-        this.SERVICE_NAME = serviceName;
-        this.PASSWORD_LENGTH = length; // Get this from the user via a GUI
-        this.SEGMENT_LENGTHS = computeSegmentLengths();
-    }
-
-    public Genesis(String serviceName, int length, JTextField textField) {
-        this.TEXT_FIELD = textField;
-        this.SERVICE_NAME = ToolBox.convertToHashSet(serviceName.toLowerCase().toCharArray());
+    public Genesis(Set<Character> serviceName, int length) {
+        this.serviceName = serviceName;
         this.PASSWORD_LENGTH = length;
         this.SEGMENT_LENGTHS = computeSegmentLengths();
     }
 
+    public Genesis(String serviceName, int length) {
+        this.serviceName = ToolBox.convertToHashSet(serviceName.toLowerCase().toCharArray());
+        this.PASSWORD_LENGTH = length;
+        this.SEGMENT_LENGTHS = computeSegmentLengths();
+    }
+
+    //---  Start of setters and getters ---
+
+    public void setServiceName(Set<Character> fullServiceName) {
+        this.serviceName = fullServiceName;
+    }
+
+    public int[] getSegmentLengths() {
+        return this.SEGMENT_LENGTHS;
+    }
+ 
+    // --- End of setters and getters ---
+    
+    @Override
+    public boolean runDiagnosis() {
+        return false;
+    }
+    
+    // TODO: Fix the run method
     @Override
     public void run() { 
-        this.TEXT_FIELD.setText(new String(getPassword()));
+        generatePassword();
     }
-        
-    public byte[] getPassword() {
+
+    /** 
+     * Responsible for obtaining the segments of the password, merging them
+     * and returning the final password.
+     *  
+     * @return A byte[] of the password. It returns a byte[] array, so that it can be eligible for encryption.
+    */
+    @Override
+    public byte[] generatePassword() {
         byte[] segmentOne = constructSegmentOne();
         byte[] segmentTwo = constructSegmentTwo();
         byte[] result2 = new byte[segmentOne.length + segmentTwo.length];
@@ -47,8 +65,14 @@ public class Genesis extends Thread implements PasswordGenerator{
         return result2;		
     }
 
+    /**
+     * Responsible for constructing the first segment of the password.
+     * This segment is dependent on the service name from the user.
+     * 
+     * @return The first segment, in the form of a byte[]
+    */
     private byte[] constructSegmentOne() {
-        final Character[] NEW_SERVICE_NAME = shuffle(trimServiceName(this.SERVICE_NAME));        
+        final Character[] NEW_SERVICE_NAME = shuffle(trimServiceName(this.serviceName));        
         final int LENGTH = NEW_SERVICE_NAME.length;
         final char[] USED_ELEMENTS = new char[LENGTH];
         final StringBuilder STRING_BUILDER = new StringBuilder(LENGTH);                
@@ -62,7 +86,14 @@ public class Genesis extends Thread implements PasswordGenerator{
         }               
         return new String(STRING_BUILDER).getBytes();		
     }
-    
+
+    /**
+     * Responsible for constructing the second segment of the password.
+     * This segment utilizes special characters and numbers.
+     * It also tries to ensure randomness in the order of the elements.
+     * 
+     * @return The second segment, in the form of a byte[]
+    */
     private byte[] constructSegmentTwo() {
         final char[] SPECIAL_CHARS = {'!', '@', '#', '$', '%', '^', '&', '*', '+', '/', ' '};
         final char[] NUMBERS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
@@ -98,10 +129,14 @@ public class Genesis extends Thread implements PasswordGenerator{
         return RESULT;
     }
 
+    /**
+     * Responsible for identifying the length of each segment.
+     * @return A lengths for both segments in an int[]. Index 0 is the segment 1 length and Index 1 is the segment 2 length
+    */
     private int[] computeSegmentLengths() {        
         final int LENGTH_PER_SEGMENT = this.PASSWORD_LENGTH / 2;
         final int SEGMENT_ONE_LENGTH = LENGTH_PER_SEGMENT;        
-        final int difference = SEGMENT_ONE_LENGTH - this.SERVICE_NAME.size();
+        final int difference = SEGMENT_ONE_LENGTH - this.serviceName.size();
         int segmentTwoLength = (this.PASSWORD_LENGTH % 2 == 0) ? LENGTH_PER_SEGMENT : LENGTH_PER_SEGMENT + 1;
         segmentTwoLength = (difference > 0) ? segmentTwoLength + difference : segmentTwoLength;        
         return new int[]{SEGMENT_ONE_LENGTH, segmentTwoLength};
@@ -116,6 +151,11 @@ public class Genesis extends Thread implements PasswordGenerator{
         return false;    
     }
    
+    /** 
+    * Responsible for trimming the service name to abide by the segment length constraint.
+    * @param source The full service name
+    * @return A Set<Characters> of the remaining (non-trimmed) characters.
+    */
     public Set<Character> trimServiceName(Set<Character> source) {        
         final Set<Character> DESTINATION = new HashSet<>();        
         final Iterator<Character> ITERATOR = source.iterator();
@@ -154,10 +194,5 @@ public class Genesis extends Thread implements PasswordGenerator{
             ARRAY[randomIndex2] = temp;            
         }
         return ARRAY;
-    }
-
-    @Override
-    public boolean runDiagnosis() {
-        return false;
     }
 }
