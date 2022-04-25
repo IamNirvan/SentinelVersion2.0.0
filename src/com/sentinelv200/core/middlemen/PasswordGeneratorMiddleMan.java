ec5f389f82dffc11d1a8c6ecd76db16cefbfc50b
Nirvan <nirvan.work@gmail.com>
2022-04-25 19:40:09 +0500

    Update PasswordGeneratorMiddleMan.java
    Made small refactors

--- src/com/sentinelv200/core/middlemen/PasswordGeneratorMiddleMan.java
+++ src/com/sentinelv200/core/middlemen/PasswordGeneratorMiddleMan.java
@@ -3,14 +3,11 @@ package com.sentinelv200.core.middlemen;
 import com.sentinelv200.core.Core;
 import com.sentinelv200.engines.passwordgenerator.PasswordGenerator;
 
+//TODO: Build this class
+
 public class PasswordGeneratorMiddleMan implements MiddleMan{
     public PasswordGenerator passwordGenerator;
     boolean connectionStatus = false;
-
-    @Override
-    public boolean locateEngine() {
-        return false;
-    }
 
     @Override
     public void createEngine() {
@@ -30,5 +27,11 @@ public class PasswordGeneratorMiddleMan implements MiddleMan{
     @Override
     public void removeEngine() {
         Core.removeEngine(passwordGenerator);
+    }
+
+    @Override
+    public void start() {
+        // TODO Auto-generated method stub
+        
     }
 }
